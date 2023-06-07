beb22f4005d4fe14f104a08ea3459bb3dc6b1394
Maiko Matsuoka <117214172+NIMUchanco@users.noreply.github.com>
2023-06-06 20:21:10 -0400

    This is the fist commit. Added php files and assets folder
    Added index.php, about.php, and assets folder which contains css folder and images folder. Created a website which introduces myself.

---
+++ about.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maiko Matsuoka</title>
+    <link rel="stylesheet" href="assets/css/style.css"/>
+    <link rel="stylesheet" href="assets/css/about.css" />
+</head>
+
+<body>
+    <header>
+        <h1 class="logo">Maiko Matsuoka</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="about.html">About me</a>
+        </nav>
+    </header>
+
+    <section class="bg">
+        <div class="container">
+            <div class="block block-1">
+                <h2>About me</h2>
+                <p>I am 28 years old and from Japan. I have been living in Montreal around 3 years.In my spare time, I learn French and
+                programming languages. I have learned HTML, CSS, and a basic of JavaScript (+ jQuery) by myselfso far.</p>
+            </div>
+
+            <div class="block block-2">
+                <h2>My work experience</h2>
+                <p>After graduating from university, I worked as a English teacher and sales representative at a language school in Japan
+                for two years. After that, I worked as a Japanese teacher in Canada for over two years. After teaching myself web design
+                and coding, I created a website for a Canadian startup company.</p>
+
+                <ul>
+                    <li>Here is a link to <a href="https://everlasting-remarkable-crustacean.glitch.me/">my very first website</a> for the startup.</li>
+                    <li>Here is a link to my <a href="https://languid-silk-sun.glitch.me/">portfolio</a>.</li>
+                </ul>
+            </div>
+
+            <div class="block block-3">
+                <h2>How I become interested in the web industry</h2>
+                <p>I originally worked in the education industry as described above andhoped to start my own business as a teacher in the
+                future. Therefore, I wanted to create my own website, and this is what got me interested in the web industry and
+                programming.</p>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Maiko Matsuoka</title>
+        <link rel="stylesheet" href="assets/css/style.css"/>
+        <link rel="stylesheet" href="assets/css/home.css"/>
+    </head>
+    <body>
+        <header>
+            <h1 class="logo">Maiko Matsuoka</h1>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="about.html">About me</a>
+            </nav>
+        </header>
+        
+        <section class="banner">
+            <video src="assets/images/flower.mp4" autoplay loop muted></video>
+
+            <div class="overlay"></div>
+            <div class="text">
+                <h2>Welcome to my website</h2>
+                <p>I'm Maiko, a student aiming to become a front-end developer.</p>
+            </div>
+
+            <ul class="icons">
+                <a href="https://www.linkedin.com/in/maiko-matsuoka-b84966255/" target="_blank"><img src="assets/images/linkedin.png" alt=""></a>
+                <a href="https://github.com/NIMUchanco" target="_blank"><img src="assets/images/github.png" alt=""></a>
+            </ul>
+        </section>
+    </body>
+</html>
